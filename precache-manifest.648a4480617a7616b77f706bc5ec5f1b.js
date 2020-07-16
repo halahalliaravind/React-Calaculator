d3f3767f8e97a9168a248e190e57ab13408fb633
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-07-16 20:57:28 +0530

    Updates

---
+++ precache-manifest.648a4480617a7616b77f706bc5ec5f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deffdc6da70cc7d4c592c7609a81ecf9",
+    "url": "/React-Calaculator/index.html"
+  },
+  {
+    "revision": "ea174ba6fe63af1c310a",
+    "url": "/React-Calaculator/static/css/main.18eb71f6.chunk.css"
+  },
+  {
+    "revision": "7c1cedb5cc9d4bb7eced",
+    "url": "/React-Calaculator/static/js/2.d1b75d3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calaculator/static/js/2.d1b75d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea174ba6fe63af1c310a",
+    "url": "/React-Calaculator/static/js/main.8e922ae1.chunk.js"
+  },
+  {
+    "revision": "502d7a452f2ddb1c1ec2",
+    "url": "/React-Calaculator/static/js/runtime-main.dfb6c117.js"
+  }
+]);
